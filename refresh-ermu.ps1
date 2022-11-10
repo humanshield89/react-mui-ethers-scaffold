7c55a3c0c3a7460ea6fd8fc816e4c6ecb6f91e3b
Rachid Boudjelida <rachidboudjelida@gmail.com>
2022-11-10 01:18:11 +0100

    Add all new features to the demo

---
+++ refresh-ermu.ps1
@@ -0,0 +1,2 @@
+yarn remove "@dailycodeltd/ermu"
+yarn add I:\\organized_projects\\react-ether-utils-parcel\\dailycodeltd-ermu-v1.0.6.tgz
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Page from "../../components/Page";
+
+const HomePage = () => {
+  return (
+    <Page title={"Home"}>
+      <h1>Home Page</h1>
+    </Page>
+  );
+};
+
+export default HomePage;
